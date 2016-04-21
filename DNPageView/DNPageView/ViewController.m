98a9bee8347a813f9805db89734e570822582a37
meteor_1990 <626600815@qq.com>
2016-04-21 09:30:01 +0800

    引导页初始化

---
+++ DNPageView/DNPageView/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  DNPageView
+//
+//  Created by mainone on 16/4/20.
+//  Copyright © 2016年 wjn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
